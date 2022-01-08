48d38ea32d62c73aeed684b60d37f2f7f1ef6823
NM-2009 <77423870+NM-2009@users.noreply.github.com>
2022-01-07 17:53:26 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+noseX = 0;
+noseY = 0;
+
+leftwristX = 0;
+rightwristX = 0;
+difference = 0;
+
+function setup()
+{
+    video=createCapture (VIDEO);
+    video.size(550, 550);
+
+    canvas = createCanvas(550,550 );
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    background("#969A97");
+    fill("yellow");
+    stroke("red");
+    square(noseX, noseY, difference);
+}
+
+function modelLoaded() {
+    console.log('Posenet is Initialized!');
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = "+ noseX + "noseY = "+ noseY);
+
+        leftwristX = results[0].pose.leftWrist.x;
+        rightwristX = results[0].pose.rightWrist.x;
+        difference = floor(leftwristX - rightwristX);
+        console.log("leftwristX =" + leftwristX + "rightwristX =" + rightwristX + "difference =" + difference);
+
+    }
+}
